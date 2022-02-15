dafd52fd01301f445d7df26e185ecd7b16368641
DaigoWakabayashi <mamushi.journey@gmail.com>
2022-02-15 23:15:17 +0900

    #1 Update :  dark theme setting in MaterialApp

--- lib/main.dart
+++ lib/main.dart
@@ -16,7 +16,8 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Demo',
-      theme: ThemeData(primarySwatch: Colors.blue),
+      theme: ThemeData.light(),
+      darkTheme: ThemeData.dark(),
       home: const MyHomePage(title: 'Flutter Demo Home Page'),
       useInheritedMediaQuery: true,
       locale: DevicePreview.locale(context),
